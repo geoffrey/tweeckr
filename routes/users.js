ae83d30a590532de9c0deb947f1a9a7fbacbfddb
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2015-03-03 22:23:31 -0800

    Add async, fetch last tweets in parallel with user profile

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,36 @@
 var express   = require('express');
 var router    = express.Router();
+var async     = require('async');
 
 var twitter   = require('../utils/twitter');
 
 
 router.get('/:screenname', function(req, res, next) {
-
-  twitter.get('users/show', {
-    screen_name: req.params.screenname
-  }, function(err, data, response) {
+  var screenname = req.params.screenname;
+  async.parallel({
+    user: function(cb) {
+      twitter.get('users/show', {
+        screen_name: screenname
+      }, cb);
+    },
+    latest_tweets: function(cb) {
+      twitter.get('statuses/user_timeline', {
+        screen_name: screenname
+      }, cb);
+    }
+  }, function(err, results) {
     if (err) {
-      return res
-      .status(err.statusCode)
-      .json({
+      return res.status(err.statusCode).json({
         code: err.statusCode,
         message: err.message
       });
     }
 
-    res.status(200).json(data);
+    res.status(200).json({
+      user: results.user[0],
+      latest_tweets: results.latest_tweets[0]
+    });
   });
-
 });
 
 
