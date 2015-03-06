7ef91094ce7502f68af4cca9243f16ad25e4145a
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2015-03-05 21:46:10 -0800

    Add shareable url

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,10 @@
-function MainCtrl($scope, API) {
+function MainCtrl($scope, $location, API) {
+
+  // Get the screenname from path - localStorage or default
+  $scope.screenname = $location.path().substr(1) || window.localStorage.screenname || 'geoffrey___';
+  $location.path($scope.screenname);
 
   var init = function() {
-    $scope.screenname = window.localStorage.screenname || '';
     $scope.user = null;
     $scope.latest_tweets = [];
     $scope.reputation_score = null;
@@ -9,53 +12,61 @@ function MainCtrl($scope, API) {
   };
 
 
+  function _getUserAndLatestTweetsWithSuccess(user, tweets) {
+    console.info('Done', arguments);
+
+    $scope.$apply(function() {
+      $scope.user = user[0].user;
+      // just to get a bigger image
+      $scope.user.profile_image_url = $scope.user.profile_image_url.replace('normal', '200x200');
+
+      $scope.latest_tweets = tweets[0].latest_tweets;
+      $scope.reputation_score = tweets[0].tweets_reputation_score +
+        user[0].user_reputation_score;
+    });
+  }
+
+
+  function _getUserAndLatestTweetsWithError(err) {
+    console.error('Fail', err);
+
+    var error;
+    switch (err.status) {
+      case 404:
+        error = 'This user does not exist.';
+        break;
+      case 401:
+        error = 'This user\'s tweets are private.';
+        break;
+      default:
+        error = 'Oops something unexpected happened. Please try again.';
+    }
+
+    $scope.$apply(function() {
+      init();
+      $scope.apiError = error;
+    });
+  }
+
+
   var getProfileAndLatestTweets = function() {
     init();
 
     if ($scope.screenname === '') return;
 
+    // Get User profile and Latest Tweets in parallel
     $.when(
       API.getUser($scope.screenname),
       API.getTweets($scope.screenname)
     )
-    .done(function getTweeckrDataWithSuccess(user, tweets) {
-      console.info('Done', arguments);
-
-      $scope.$apply(function() {
-        $scope.user = user[0].user;
-        // just to get a bigger image
-        $scope.user.profile_image_url = $scope.user.profile_image_url.replace('normal', '200x200');
-
-        $scope.latest_tweets = tweets[0].latest_tweets;
-        $scope.reputation_score = tweets[0].tweets_reputation_score +
-          user[0].user_reputation_score;
-      });
-    })
-    .fail(function(err) {
-      console.error('Fail', err);
-
-      var error;
-      switch (err.status) {
-        case 404:
-          error = 'This user does not exist.';
-          break;
-        case 401:
-          error = 'This user\'s tweets are private.';
-          break;
-        default:
-          error = 'Oops something unexpected happened. Please try again.';
-      }
-
-      $scope.$apply(function() {
-        init();
-        $scope.apiError = error;
-      });
-    });
+    .done(_getUserAndLatestTweetsWithSuccess)
+    .fail(_getUserAndLatestTweetsWithError);
   };
 
 
   $scope.check = function() {
     window.localStorage.screenname = $scope.screenname;
+    $location.path($scope.screenname);
     getProfileAndLatestTweets();
   };
 
